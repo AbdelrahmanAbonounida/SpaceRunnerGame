7b480109ae2e2f61dfff83e348827c5abc03a0e4
Abdelrahman Abounida <abdelrahmanaboneda@gmail.com>
2021-12-11 06:15:04 +0200

    SpaceRunner with javaFX

--- Space Runner/src/application/Main.java
+++ Space Runner/src/application/Main.java
@@ -8,8 +8,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-			
+						
 			ViewManager manager = new ViewManager();
 			
 			Stage s = manager.getStage();
