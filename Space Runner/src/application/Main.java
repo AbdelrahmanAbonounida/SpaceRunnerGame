5d13877b68df1a3f5a805d6abbe34a182eb822c9
Abdelrahman Abounida <abdelrahmanaboneda@gmail.com>
2021-12-11 05:39:52 +0200

    main game

--- Space Runner/src/application/Main.java
+++ Space Runner/src/application/Main.java
@@ -8,7 +8,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-
+			
+			
 			ViewManager manager = new ViewManager();
 			
 			Stage s = manager.getStage();
